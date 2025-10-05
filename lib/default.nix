09c7ae87ce7d7af846fcddd4fff7996eebd09cad
SushyDev <mail@sushy.dev>
2025-10-05 13:50:35 +0200

    improve lib

--- lib/default.nix
+++ lib/default.nix
@@ -11,36 +11,35 @@ let
 			runHook preUnpack
 
 			function attach_disk_image() {
-				local SOURCE_IMAGE="$1"
-				hdiutil attach -readonly -nobrowse "$SOURCE_IMAGE"
+				local -r source_image="$1"
+				echo "Attaching disk image: $source_image" >&2
+				hdiutil attach -readonly -nobrowse "$source_image"
 			}
 
 			function extract_mount_point() {
-				local HDIUTIL_OUTPUT="$1"
-				echo "$HDIUTIL_OUTPUT" | awk '/\/dev\/disk/ { $1=""; $2=""; print $0 }' | xargs
+				local -r hdiutil_output="$1"
+				echo "Extracting mount point from hdiutil output" >&2
+				echo "$hdiutil_output" | awk '/\/dev\/disk/ { $1=""; $2=""; print $0 }' | xargs
 			}
 
 			function copy_files() {
-				local SOURCE_DIR="$1"
-				local DESTINATION_DIR="$2"
-				find "$SOURCE_DIR" -maxdepth 2 -name "*.app" -exec cp -R {} "$DESTINATION_DIR" \;
+				local -r source_dir="$1"
+				local -r destination_dir="$2"
+				find "$source_dir" -maxdepth 2 -name "*.app" -exec cp -R {} "$destination_dir" \;
 			}
 
 			function cleanup_mount_point() {
-				local POINT_TO_DETACH="$1"
-
-				echo "Detaching disk image at $POINT_TO_DETACH"
-				hdiutil detach -quiet "$POINT_TO_DETACH"
+				local -r point_to_detach="$1"
+				echo "Detaching disk image at $point_to_detach" >&2
+				hdiutil detach -quiet "$point_to_detach"
 			}
 
 			PATH="$PATH:/usr/bin"
 
-			echo "Attaching disk image: $src"
-			DISK_IMAGE_INFO=$(attach_disk_image "$src")
+			readonly DISK_IMAGE_INFO=$(attach_disk_image "$src")
 			echo "Disk image info: $DISK_IMAGE_INFO"
 
-			echo "Extracting mount point from hdiutil output"
-			MOUNT_POINT=$(extract_mount_point "$DISK_IMAGE_INFO")
+			readonly MOUNT_POINT=$(extract_mount_point "$DISK_IMAGE_INFO")
 			echo "Mount point: $MOUNT_POINT"
 
 			if [[ -z "$MOUNT_POINT" || ! -d "$MOUNT_POINT" ]]; then
