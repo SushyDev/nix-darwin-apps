3a5f780936fce278b579f720f1822090f4626e17
Sushy <43114803+SushyDev@users.noreply.github.com>
2025-11-03 09:49:13 +0100

    Update ghostty version to 1.2.3

--- apps/by-name/gh/ghostty/default.nix
+++ apps/by-name/gh/ghostty/default.nix
@@ -2,11 +2,11 @@
 
 lib.mkDmgApp rec {
 	pname = "ghostty";
-	version = "1.1.3";
+	version = "1.2.3";
 
 	src = pkgs.fetchurl { 
-		url = "https://release.files.ghostty.org/${version}/Ghostty.dmg";
-		sha256 = "0ir69yhqia8yj2i750g2aklk7wr9vzwbdp6ncvqri5aliwc19rb4";
+		url = "https://release.files.ghostty.org/1.2.3/Ghostty.dmg";
+		sha256 = "0sr0hg28aafd5lx8izq7ni25nmy7k18g9ppqp5x04a3f24gyjppk";
 	};
 
 	meta = with pkgs.lib; {
