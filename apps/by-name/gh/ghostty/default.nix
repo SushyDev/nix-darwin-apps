9a41a4611c37c07cb6ff9ca5b0cdcb0144a985d8
Sushy <43114803+SushyDev@users.noreply.github.com>
2026-06-16 23:36:48 +0200

    Feature/bump flake (#189)
    * sign commits
    
    * bump flake.lock
    
    * updates

--- apps/by-name/gh/ghostty/default.nix
+++ apps/by-name/gh/ghostty/default.nix
@@ -4,8 +4,8 @@ lib.mkDmgApp rec {
 	pname = "ghostty";
 	version = "1.3.1";
 
-	src = pkgs.fetchurl { 
-		url = "https://release.files.ghostty.org/1.2.3/Ghostty.dmg";
+	src = pkgs.fetchurl {
+		url = "https://release.files.ghostty.org/${version}/Ghostty.dmg";
 		sha256 = "0saziwxpkjqy5issl57jp902l9cah170dly7v7m0xsfflsqg5kqq";
 	};
 
